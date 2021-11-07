736265115cc3013e56a9df3b2042dba421257615
Gix73 <gix73@yandex.ru>
2021-11-07 21:53:34 +0300

    feat: add third and fourh tasks

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require("fs");
+const path = require("path");
 
+async function main(){
+  srcPath = path.join(__dirname, "./files/");
+  newPath = path.join(__dirname, "./files-copy/");
+
+  await fs.promises.rm(newPath, {recursive: true, force: true});
+  fs.promises.mkdir(newPath, {recursive: true});
+
+  fs.readdir(srcPath, {withFileTypes: true}, (err, files) => {
+    async function getFile(file){
+    
+      if (file.isFile()) {
+        fs.promises.copyFile(srcPath + file.name, newPath + file.name);
+      }
+    }
+  
+    files.forEach(getFile);
+  });
+}
+
+main();
